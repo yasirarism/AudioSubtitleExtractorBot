d070f001fdf3ef4d59e2dd81c6909e285bd94449
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-11-16 10:00:23 +0330

    Update download.py

--- helpers/download.py
+++ helpers/download.py
@@ -5,10 +5,11 @@
 
 import time
 import json
-
+import os
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.progress import progress_func
 from helpers.tools import execute, clean_up
+from helpers.download_from_url import download_file
 
 DATA = {}
 
@@ -42,6 +43,81 @@ async def download_file(client, message):
 
     await msg.edit_text("Processing your file....")
 
+    output = await execute(f"ffprobe -hide_banner -show_streams -print_format json '{download_location}'")
+    
+    if not output:
+        await clean_up(download_location)
+        await msg.edit_text("Some Error Occured while Fetching Details...")
+        return
+
+    details = json.loads(output[0])
+    buttons = []
+    DATA[f"{message.chat.id}-{msg.message_id}"] = {}
+    for stream in details["streams"]:
+        mapping = stream["index"]
+        stream_name = stream["codec_name"]
+        stream_type = stream["codec_type"]
+        if stream_type in ("audio", "subtitle"):
+            pass
+        else:
+            continue
+        try: 
+            lang = stream["tags"]["language"]
+        except:
+            lang = mapping
+        
+        DATA[f"{message.chat.id}-{msg.message_id}"][int(mapping)] = {
+            "map" : mapping,
+            "name" : stream_name,
+            "type" : stream_type,
+            "lang" : lang,
+            "location" : download_location
+        }
+        buttons.append([
+            InlineKeyboardButton(
+                f"{stream_type.upper()} - {str(lang).upper()}", f"{stream_type}_{mapping}_{message.chat.id}-{msg.message_id}"
+            )
+        ])
+
+    buttons.append([
+        InlineKeyboardButton("CANCEL",f"cancel_{mapping}_{message.chat.id}-{msg.message_id}")
+    ])    
+
+    await msg.edit_text(
+        "**Select the Stream to be Extracted...**",
+        reply_markup=InlineKeyboardMarkup(buttons)
+        )
+
+async def download_url_link(client, message):
+    
+    m = message.reply_to_message
+    link = m.text
+    
+    if '|' in link:
+        link, filename = link.split('|')
+        link = link.strip()
+        filename = filename.strip()
+        dl_path = os.path.join(f'{DOWNLOAD_DIRECTORY}{filename}')
+    else:
+        link = link.strip()
+        filename = os.path.basename(link)
+        dl_path = os.path.join(DOWNLOAD_DIRECTORY, os.path.basename(link))
+    
+    msg = await client.send_message(
+        chat_id=m.chat.id,
+        text="**Downloading your Link to Server...**",
+        reply_to_message_id=m.message_id
+    )
+    
+    start = time.time()
+    try:
+        download_location = await download_file(link, dl_path, msg, start, client)
+    except Exception as e:
+        print(e)
+        await msg.edit(f"**Download Failed** :\n\n{e}")
+        await clean_up(download_location)
+        return
+    
     output = await execute(f"ffprobe -hide_banner -show_streams -print_format json '{download_location}'")
     
     if not output:
