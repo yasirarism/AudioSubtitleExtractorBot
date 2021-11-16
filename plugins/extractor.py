80dc2f75092122bd3c835d0a13208a4d862ce1da
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-11-16 09:44:42 +0330

    Update extractor.py

--- plugins/extractor.py
+++ plugins/extractor.py
@@ -35,3 +35,19 @@ async def confirm_dwnld(client, message):
             "Invalid Media",
             quote=True
         )
+@trojanz.on_message(filters.private & filters.incoming & filters.text & (filters.regex('^(ht|f)tp*')))
+async def link_dwnld(client, message):
+
+    if message.from_user.id not in Config.AUTH_USERS:
+        await message.reply_text(text=f"sorry ! this is a bot for private use.", quote=True)
+        return
+    if not message.media:
+        #link = message.text
+        await message.reply_text(
+            "**What you want me to do??**",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(text="DOWNLOAD From Link", callback_data="download_url")],
+                [InlineKeyboardButton(text="CANCEL", callback_data="close")]
+            ])
+        )
