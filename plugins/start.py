ce2370a0092e5bb1a4d859a37ecb9ada16b2f535
yasir <yasiramunandar@gmail.com>
2022-07-26 20:56:37 +0700

    update

--- plugins/start.py
+++ plugins/start.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # @trojanzhex
 
-
 from pyrogram import filters
 from pyrogram import Client as trojanz
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -16,16 +15,11 @@ async def start(client, message):
     await message.reply_text(
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("HELP", callback_data="help_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-    )
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton("HELP", callback_data="help_data"),
+            InlineKeyboardButton("ABOUT", callback_data="about_data"),
+        ]]),
+        reply_to_message_id=message.id)
 
 
 @trojanz.on_message(filters.command(["help"]) & filters.private)
@@ -33,16 +27,11 @@ async def help(client, message):
     await message.reply_text(
         text=Script.HELP_MSG,
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("BACK", callback_data="start_data"),
-                    InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-    )
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton("BACK", callback_data="start_data"),
+            InlineKeyboardButton("ABOUT", callback_data="about_data"),
+        ]]),
+        reply_to_message_id=message.id)
 
 
 @trojanz.on_message(filters.command(["about"]) & filters.private)
@@ -50,13 +39,8 @@ async def about(client, message):
     await message.reply_text(
         text=Script.ABOUT_MSG,
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("BACK", callback_data="help_data"),
-                    InlineKeyboardButton("START", callback_data="start_data"),
-                ]
-            ]
-        ),
-        reply_to_message_id=message.message_id
-    )
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton("BACK", callback_data="help_data"),
+            InlineKeyboardButton("START", callback_data="start_data"),
+        ]]),
+        reply_to_message_id=message.id)
